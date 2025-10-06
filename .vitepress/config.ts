015d60d650166607317888477e2930bc6f58bee3
風間青祢 <101449500+adproqwq@users.noreply.github.com>
2025-10-06 13:51:42 +0800

    fix: base url

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress';
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   srcDir: 'docs',
+  base: '/docs/',
 
   title: 'QwQNT',
   description: 'QwQNT 社区驱动文档',
