bce589fc52846e9b42a9c4a8b736741529ea0e4e
風間青祢 <101449500+adproqwq@users.noreply.github.com>
2025-10-08 12:34:58 +0800

    docs: Github link

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/QwQ-002/' },
+      { icon: 'github', link: 'https://github.com/qwqnt-community/' },
     ],
   },
 });
